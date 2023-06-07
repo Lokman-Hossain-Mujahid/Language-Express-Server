7a2f526b956d310b8c4c49a39491e8408af08e99
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-08 00:18:49 +0600

    API created for dashboard role rendering

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+  // bearer
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uhtjylk.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,6 +44,12 @@ async function run() {
 
     const database = client.db("LanguageDb")
     const usersCollection = database.collection('user')
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+      res.send({token})
+    })
 
     // Users API
 
@@ -49,6 +72,27 @@ async function run() {
         console.log(result);
         res.send(result);
     })
+
+    app.get('/users/admin/:email', verifyJWT, async(req, res) => {
+      const email = req.params.email
+
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin'}
+      res.send(result);
+    })
+
+    app.get('/currentuser/:email', async(req,res) => {
+      const email = req.params.email
+      // console.log(email);
+      const result = await usersCollection.find({email: email}).toArray()
+      res.send(result);
+    })
+
 
     // TO MAKE ADMIN
     app.patch('/users/admin/:id', async(req,res) => {
